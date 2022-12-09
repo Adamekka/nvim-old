9fb593588130882a32255a6a50ca547ceb2bf37e
Adam Cvikl <adam.wasd4@gmail.com>
2022-12-09 17:26:28 +0100

    add nvim-web-devicons, sakura, lualine

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,7 +8,31 @@ return require('packer').startup({
     function(use)
         -- Package manager
         use('wbthomason/packer.nvim')
+
         -- Required plugins
         use('nvim-lua/plenary.nvim')
+
+        -- Theme, Icons, Statusbar, Bufferbar
+        use({
+            'kyazdani42/nvim-web-devicons',
+            config = function()
+                require('nvim-web-devicons').setup()
+            end,
+        })
+        use({
+            'numToStr/Sakura.nvim',
+            config = function()
+                require('numToStr.plugins.sakura')
+            end,
+        })
+        use({
+            'nvim-lualine/lualine.nvim',
+            after = 'Sakura.nvim',
+            event = 'BufEnter',
+            config = function()
+                require('numToStr.plugins.lualine')
+            end,
+        })
+
     end
 })
