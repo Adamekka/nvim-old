45ab949c1779cd9fc7bc7a256692fff3c12d06f7
Adam Cvikl <adam.wasd4@gmail.com>
2022-12-09 16:55:20 +0100

    add packer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,9 +1,14 @@
 -- vim.api.nvim_create_autocmd('BufWritePos', {
-    -- group = vim.api.nvim_create_augroup('PACKER', { clear = true }),
-    -- pattern = 'plugins.lua',
-    -- command = 'source <afile> | PackerCompile',
+--     group = vim.api.nvim_create_augroup('PACKER', { clear = true }),
+--     pattern = 'plugins.lua',
+--     command = 'source <afile> | PackerCompile',
 -- })
 
--- return require('packer').startup({
-
--- })
+return require('packer').startup({
+    function(use)
+        -- Package manager
+        use('wbthomason/packer.nvim')
+        -- Required plugins
+        use('nvim-lua/plenary.nvim')
+    end
+})
